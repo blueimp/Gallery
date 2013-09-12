f90879fe06d6026cf74e50a444e73f83e7a194b4
Sebastian Tschan <mail@blueimp.net>
2013-09-12 12:54:32 -0500

    Bind the click handler to the document node to allow including the jQuery plugin in the document head.

--- js/jquery.blueimp-gallery.js
+++ js/jquery.blueimp-gallery.js
@@ -1,5 +1,5 @@
 /*
- * blueimp Gallery jQuery plugin 1.1.0
+ * blueimp Gallery jQuery plugin 1.1.1
  * https://github.com/blueimp/Gallery
  *
  * Copyright 2013, Sebastian Tschan
@@ -29,7 +29,7 @@
 
     // Global click handler to open links with data-gallery attribute
     // in the Gallery lightbox:
-    $(document.body).on('click', '[data-gallery]', function (event) {
+    $(document).on('click', '[data-gallery]', function (event) {
         // Get the container id from the data-gallery attribute:
         var id = $(this).data('gallery'),
             widget = $(id),
