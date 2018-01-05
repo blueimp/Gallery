a16e6e3a8ecdecec313b995a6de3cb3bf02ecf1d
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-05 09:43:57 -0800

    add jquery as a peer and as an external module

--- js/src/main-jquery.js
+++ js/src/main-jquery.js
@@ -4,17 +4,13 @@ import './blueimp-gallery-video'
 import './blueimp-gallery-vimeo'
 import './blueimp-gallery-youtube'
 
-import helper from './blueimp-helper'
-import Gallery from './blueimp-gallery'
+import galleryFactory from './jquery.blueimp-gallery'
+
+const Gallery = galleryFactory()
 
 if (typeof window !== 'undefined') {
   window.blueimp = window.blueimp || {}
   window.blueimp.Gallery = Gallery
-
-  // use helper if jQuery isn't present
-  if (typeof window.jQuery === 'undefined') {
-    window.$ = helper
-  }
 }
 
 export default Gallery
--- rollup.config.jquery.js
+++ rollup.config.jquery.js
@@ -1,18 +1,19 @@
 import resolve from 'rollup-plugin-node-resolve'
+
 import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify'
 
 export default {
-  input: 'js/src/main.js',
+  input: 'js/src/main-jquery.js',
   plugins: [
     resolve(),
     buble(),
     uglify()
   ],
   output: {
-    file: 'js/dist/blueimp-gallery.min.js',
-    name: 'blueimp-gallery',
+    file: 'js/dist/jquery.blueimp-gallery.min.js',
+    name: 'blueimp-gallery-jquery',
     format: 'umd'
   },
-  external: ['jquery']
+  external: ['jquery', '$']
 }
