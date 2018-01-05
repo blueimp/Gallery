1339edc248631bc84fe483db896a5456debf8198
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-05 10:08:47 -0800

    replace this references to fix jquery plugin

--- js/src/jquery.blueimp-gallery.js
+++ js/src/jquery.blueimp-gallery.js
@@ -14,53 +14,52 @@
 import $ from 'jquery'
 import Gallery from './blueimp-gallery'
 
-export default function () {
-  // Global click handler to open links with data-gallery attribute
-  // in the Gallery lightbox:
+// Global click handler to open links with data-gallery attribute
+// in the Gallery lightbox:
 
-  $(document).on('click', '[data-gallery]', (event) => {
-    // Get the container id from the data-gallery attribute:
-    const id = $(this).data('gallery')
-    const widget = $(id)
-    const container = (widget.length && widget) || $(Gallery.prototype.options.container)
+$(document).on('click', '[data-gallery]', (event) => {
+  event.preventDefault()
 
-    const callbacks = {
-      onopen: function () {
-        container.data('gallery', this).trigger('open')
-      },
-      onopened: function () {
-        container.trigger('opened')
-      },
-      onslide: function () {
-        container.trigger('slide', arguments)
-      },
-      onslideend: function () {
-        container.trigger('slideend', arguments)
-      },
-      onslidecomplete: function () {
-        container.trigger('slidecomplete', arguments)
-      },
-      onclose: function () {
-        container.trigger('close')
-      },
-      onclosed: function () {
-        container.trigger('closed').removeData('gallery')
-      }
+  // Get the container id from the data-gallery attribute:
+  const id = $(event.target).data('gallery')
+  const widget = $(id)
+  const container = (widget.length && widget) || $(Gallery.prototype.options.container)
+
+  const callbacks = {
+    onopen: function () {
+      container.data('gallery', Gallery).trigger('open')
+    },
+    onopened: function () {
+      container.trigger('opened')
+    },
+    onslide: function () {
+      container.trigger('slide', arguments)
+    },
+    onslideend: function () {
+      container.trigger('slideend', arguments)
+    },
+    onslidecomplete: function () {
+      container.trigger('slidecomplete', arguments)
+    },
+    onclose: function () {
+      container.trigger('close')
+    },
+    onclosed: function () {
+      container.trigger('closed').removeData('gallery')
     }
+  }
 
-    const options = $.extend(
-      // Retrieve custom options from data-attributes
-      // on the Gallery widget:
-      container.data(),
-      { container: container[0], index: this, event: event },
-      callbacks
-    )
+  const options = $.extend(
+    // Retrieve custom options from data-attributes
+    // on the Gallery widget:
+    container.data(),
+    { container: container[0], index: event.currentTarget, event: event },
+    callbacks
+  )
 
-    // Select all links with the same data-gallery attribute:
-    let links = $(`[data-gallery=${id}]`)
+  // Select all links with the same data-gallery attribute:
+  let links = (id !== undefined) ? $(`[data-gallery=${id}]`) : $('[data-gallery]')
+  if (options.filter) links = links.filter(options.filter)
 
-    if (options.filter) links = links.filter(options.filter)
-
-    return new Gallery(links, options)
-  })
-}
+  return new Gallery(links, options)
+})
--- js/src/main-jquery.js
+++ js/src/main-jquery.js
@@ -3,10 +3,9 @@ import './blueimp-gallery-indicator'
 import './blueimp-gallery-video'
 import './blueimp-gallery-vimeo'
 import './blueimp-gallery-youtube'
+import './jquery.blueimp-gallery'
 
-import galleryFactory from './jquery.blueimp-gallery'
-
-const Gallery = galleryFactory()
+import Gallery from './blueimp-gallery'
 
 if (typeof window !== 'undefined') {
   window.blueimp = window.blueimp || {}
