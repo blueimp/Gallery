7e8884e320dfa6ecba27626fb8fcb989d22ba9c3
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-04 16:35:00 -0800

    move .js files to src, and min.js to dist
    - update main in package.json
    - update input to correct directory

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify'
 
 export default {
-  input: 'js/main.js',
+  input: 'js/src/main.js',
   plugins: [
     buble(),
     uglify()
   ],
   output: {
-    file: 'js/blueimp-gallery.min.js',
+    file: 'js/dist/blueimp-gallery.min.js',
     name: 'blueimp-gallery',
     format: 'umd'
   }
